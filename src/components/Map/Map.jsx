0e7621d948ce12d846f7182ff2ca2a65fa4a63d0
Renato Delboni <renatonato_jr@hotmail.com>
2022-02-07 12:06:18 +0900

    innitial layout done

---
+++ src/components/Map/Map.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styled from 'styled-components';
+
+
+const Map = () => {
+  return (
+    <div>
+      <h1>Map</h1>
+    </div>
+  );
+};
+
+export default Map;
---
+++ src/components/PlaceDetails/PlaceDetails.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styled from 'styled-components';
+
+
+const PlaceDetails = () => {
+  return (
+    <div>
+      <h1>Details</h1>
+    </div>
+  );
+};
+
+export default PlaceDetails;
