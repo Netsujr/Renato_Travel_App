47cf4e465522f9c77dd2c87a7c10a4582b0b6770
Renato Delboni <renatonato_jr@hotmail.com>
2022-02-07 13:46:08 +0900

    added styles
    added styles using MakeStyles

--- src/components/PlaceDetails/PlaceDetails.jsx
+++ src/components/PlaceDetails/PlaceDetails.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import styled from 'styled-components';
+import useStyles from './styles';
 
 
 const PlaceDetails = () => {
+  const classes = useStyles();
   return (
     <div>
       <h1>Details</h1>
