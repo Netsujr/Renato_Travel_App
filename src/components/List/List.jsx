bb79e5b312e21db882a67b11d3f567665046c8c8
Renato Delboni <renatonato_jr@hotmail.com>
2022-02-08 12:09:52 +0900

    added API

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -24,7 +24,7 @@ const List = () => {
       <Typography variant="h6">Restaurants, Hotels & Attractions</Typography>
       <FormControl className={classes.formControl}>
         <InputLabel>Type</InputLabel>
-        <Select value={type} onChange={(event) => setType(event.target.value)}>
+        <Select id='type' value={type} onChange={(event) => setType(event.target.value)}>
           <MenuItem value="restaurants">Restaurants</MenuItem>
           <MenuItem value="hotels">Hotels</MenuItem>
           <MenuItem value="attractions">Attractions</MenuItem>
@@ -32,7 +32,7 @@ const List = () => {
       </FormControl>
       <FormControl className={classes.formControl}>
         <InputLabel>Rating</InputLabel>
-        <Select value={rating} onChange={(event) => setRating(event.target.value)}>
+        <Select id="rating" value={rating} onChange={(event) => setRating(event.target.value)}>
           <MenuItem value={0}>All</MenuItem>
           <MenuItem value={3}>Above 3.0</MenuItem>
           <MenuItem value={4}>Above 4.0</MenuItem>
@@ -41,7 +41,7 @@ const List = () => {
       </FormControl>
       <Grid container spacing={3} className={classes.list}>
         {places?.map((place, i) => (
-          <Grid item xs={12} sm={6} md={4} lg={3} key={i}>
+          <Grid item key={i} xs={12}>
             <PlaceDetails place={place}/>
           </Grid>
 
