e6f27f277d9d0fe53b44720720cbb8920a1eb4f8
Renato Delboni <renatonato_jr@hotmail.com>
2022-02-09 17:50:12 +0900

    updated app

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -4,7 +4,7 @@ import { Paper, Typography, useMediaQuery } from '@material-ui/core';
 import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
 import Rating from '@material-ui/lab/Rating';
 
-// import mapStyles from '../../mapStyles';
+import mapStyles from '../../mapStyles';
 import useStyles from './styles.js';
 
 const Map = ({ coords, places, setCoords, setBounds, setChildClicked, weatherData }) => {
@@ -14,14 +14,12 @@ const Map = ({ coords, places, setCoords, setBounds, setChildClicked, weatherDat
   return (
     <div className={classes.mapContainer}>
       <GoogleMapReact
-        bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAPS_API_KEY }}
+        bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAP_API_KEY }}
         defaultCenter={coords}
         center={coords}
         defaultZoom={14}
         margin={[50, 50, 50, 50]}
-        options={{ disableDefaultUI: true, zoomControl: true,
-          // styles: mapStyles
-        }}
+        options={{ disableDefaultUI: true, zoomControl: true, styles: mapStyles }}
         onChange={(e) => {
           setCoords({ lat: e.center.lat, lng: e.center.lng });
           setBounds({ ne: e.marginBounds.ne, sw: e.marginBounds.sw });
