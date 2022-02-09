463f5aaba6af800c68707daa8ee68c136ffeec93
Renato Delboni <renatonato_jr@hotmail.com>
2022-02-09 14:31:05 +0900

    added Map cards

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -7,9 +7,9 @@ import LocationOutinedIcon from '@material-ui/icons/LocationOnOutlined';
 import Rating from '@material-ui/lab/Rating';
 
 
-const Map = ({ setCoords, setBounds, coords}) => {
+const Map = ({ setCoords, setBounds, coords, places }) => {
   const classes = useStyles();
-  const isMobile = useMediaQuery('(min-width:600px)');
+  const isDesktop = useMediaQuery('(min-width:600px)');
 
 
   return (
@@ -19,19 +19,47 @@ const Map = ({ setCoords, setBounds, coords}) => {
         defaultCenter={coords}
         center={coords}
         defaultZoom={14}
-        margin={ [50, 50, 50, 50] }
+        margin={[50, 50, 50, 50]}
         options={''}
         onChange={(e) => {
           // console.log(e)
-          setCoords({lat: e.center.lat, lng: e.center.lng });
+          setCoords({ lat: e.center.lat, lng: e.center.lng });
           setBounds({ ne: e.marginBounds.ne, sw: e.marginBounds.sw });
         }}
         onChildClick={() => { '' }}
       >
+        {places?.map((place, i) => (
+          <div
+            className={classes.marker}
+            lat={Number{place.latitude}}
+        lng={Number{place.longitude}}
+        key={i}
+        style={{}}
+      >
 
-      </GoogleMapReact>
+        {
+          isDesktop ?
+            <LocationOutinedIcon color={primary} style={{ fontSize: '2rem' }} />
+            :
+            <Paper elevation={3} className={classes.paper}>
+              <Typography className={classes.typography} variant='subtitle2' gutterBottom>
+                {place.name}
+              </Typography>
+              <img className={classes.pointer} src={place.image_url} alt={place.name}
+                src={place.photo ? place.photo.images.large.url : 'https://www.foodserviceandhospitality.com/wp-content/uploads/2016/09/Restaurant-Placeholder-001.jpg'}
+                alt={place.name}
+
+              />
+
+            </Paper>
+        }
     </div>
-  );
-};
+  ))
+}
+
+      </GoogleMapReact >
+      </div >
+      );
+    };
 
 export default Map;
