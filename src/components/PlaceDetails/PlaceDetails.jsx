9cbef1ecfa17df65fc522446b26a5926735b616a
Renato Delboni <renatonato_jr@hotmail.com>
2022-02-10 16:28:25 +0900

    style changes

--- src/components/PlaceDetails/PlaceDetails.jsx
+++ src/components/PlaceDetails/PlaceDetails.jsx
@@ -3,6 +3,8 @@ import { Box, Typography, Button, Card, CardMedia, CardContent, CardActions, Chi
 import LocationOnIcon from '@material-ui/icons/LocationOn';
 import PhoneIcon from '@material-ui/icons/Phone';
 import Rating from '@material-ui/lab/Rating';
+import LanguageIcon from '@material-ui/icons/Language';
+import InfoIcon from '@material-ui/icons/Info';
 
 import useStyles from './styles';
 
@@ -19,15 +21,20 @@ const PlaceDetails = ({ place, selected, refProp }) => {
         title={place.name}
       />
       <CardContent>
-        <Typography gutterBottom variant="h5">{place.name}</Typography>
-        <Box display="flex" justifyContent="space-between">
-          <Rating value={Number(place.rating)} readOnly />
-          <Typography gutterBottom variant="subtitle1">out of {place.num_reviews} reviews</Typography>
+        <Box display="flex" justifyContent="space-between" alignItems='flex-start'>
+          <Typography gutterBottom variant="h6">{place.name}</Typography>
+          <Box display="flex" flexDirection='column' marginTop='.2em'>
+            <Rating value={Number(place.rating)} readOnly />
+            <Typography style={{ marginLeft: '.35em' }} gutterBottom variant="subtitle2">{place.num_reviews} reviews</Typography>
+          </Box>
         </Box>
-        <Box display="flex" justifyContent="space-between">
-          <Typography variant="subtitle1">Price</Typography>
-          <Typography gutterBottom variant="subtitle1">{place.price_level}</Typography>
+        <Box display="flex" alignItems='center'>
+          <Typography variant="subtitle1">Price: </Typography>
+          <Typography style={{ margin: '0 .35em' }} gutterBottom variant="subtitle1">{place.price_level ? place.price_level : "No Price Info"}</Typography>
         </Box>
+          {place?.cuisine?.map(({ name }) => (
+            <Chip key={name} size="small" label={name} className={classes.chip} />
+          ))}
 
         {place?.awards?.map((award) => (
           <Box my={1} display="flex" justifyContent={'space-between'}>
@@ -35,31 +42,28 @@ const PlaceDetails = ({ place, selected, refProp }) => {
             <Typography variant="subtitle2" color="textSecondary">{award.display_name}</Typography>
           </Box>
         ))}
-        {place?.cuisine?.map(({ name }) => (
-          <Chip key={name} size="small" label={name} className={classes.chip} />
-        ))}
         {place?.address && (
           <Typography gutterBottom variant="body2" color="textSecondary" className={classes.subtitle}>
             <LocationOnIcon /> {place.address}
           </Typography>
         )}
-        <Box display="flex" justifyContent="space-between">
+        <Box display="flex" justifyContent="space-between" alignItems='flex-end'>
           {place?.phone && (
             <Typography gutterBottom variant="body2" color="textSecondary" className={classes.spacing}>
               <PhoneIcon /> {place.phone}
             </Typography>
           )}
+          <Box display="flex">
+            <Button size="small" color="primary" style={{ minWidth: 'fit-content' }} onClick={() => window.open(place.web_url, '_blank')}>
+              <InfoIcon />
+            </Button>
+            <Button size="small" color="primary" style={{ minWidth: 'fit-content' }} onClick={() => window.open(place.website, '_blank')}>
+              <LanguageIcon />
+            </Button>
+          </Box>
         </Box>
-        <CardActions>
-          <Button size="small" color="primary" onClick={() => window.open(place.web_url, '_blank')}>
-            Trip Advisor
-          </Button>
-          <Button size="small" color="primary" onClick={() => window.open(place.website, '_blank')}>
-            Website
-          </Button>
-        </CardActions>
       </CardContent>
-    </Card>
+    </Card >
   );
 };
 
